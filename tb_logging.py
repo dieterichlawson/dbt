d6c57a86ceded856012c6ab130ef77014e62bcdc
Dieterich Lawson <dieterich.lawson@gmail.com>
2020-05-03 11:57:49 -0400

    Added image logging

--- tb_logging.py
+++ tb_logging.py
@@ -4,28 +4,13 @@ import io
 
 class TensorboardLogger(object):
 
-    def __init__(self, log_dir):
-        self.writer = tf.summary.create_file_writer(log_dir)
+  def __init__(self, log_dir):
+    self.writer = tf.summary.create_file_writer(log_dir)
 
-    def log_scalar(self, tag, value, step):
-        with self.writer.as_default():
-            tf.summary.scalar(tag, value, step=step)
+  def log_scalar(self, tag, value, step):
+    with self.writer.as_default():
+      tf.summary.scalar(tag, value, step=step)
 
-#    def log_images(self, tag, images, step):
-#        im_summaries = []
-#        for nr, img in enumerate(images):
-#            # Write the image to a string
-#            s = io.BytesIO()
-#            plt.imsave(s, img, format='png')
-#
-#            # Create an Image object
-#            img_sum = tf.Summary.Image(encoded_image_string=s.getvalue(),
-#                                       height=img.shape[0],
-#                                       width=img.shape[1])
-#            # Create a Summary value
-#            im_summaries.append(tf.Summary.Value(tag='%s/%d' % (tag, nr),
-#                                                 image=img_sum))
-#
-#        # Create and write Summary
-#        summary = tf.Summary(value=im_summaries)
-#        self.writer.add_summary(summary, step)
+  def log_images(self, tag, images, step):
+    with self.writer.as_default():
+      tf.summary.image(tag, images, step=step, max_outputs=100)
